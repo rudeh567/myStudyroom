c2a262f8f40e55bdad4c6b84c6baa6e617232fee
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-06-25 10:25:54 +0900

    Hello World
    제 2장 Hello World 앱을 만들었다.

--- FirstStudy/FirstStudy/ViewController.swift
+++ FirstStudy/FirstStudy/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var hello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnSend(_ sender: UIButton) {
+        hello.text = "Hello, " + txtName.text!
+    }
+    
 
 }
 
