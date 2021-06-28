fffe069d7af6b029a399c18264b15c1279a2aca4
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-06-29 08:45:26 +0900

    Alert 프로젝트 생성

---
+++ Alert/Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by GSM08 on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
