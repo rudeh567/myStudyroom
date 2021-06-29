2d34081cf676c07e03cfb0e00cb6e6b0d64a36b0
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-06-29 12:19:00 +0900

    웹앱 만들기

---
+++ WebApp/WebApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WebApp
+//
+//  Created by GSM08 on 2021/06/29.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet var myWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
+        let myUrl = URL(fileURLWithPath: filePath!)
+        let myRequest = URLRequest(url: myUrl)
+        myWebView.load(myRequest)
+    }
+
+
+}
+
