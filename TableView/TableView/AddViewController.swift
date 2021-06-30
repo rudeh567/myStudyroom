e8f7544c8b38d607040db0d519ae6275371422fa
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-06-30 21:13:48 +0900

    todolist

---
+++ TableView/TableView/AddViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddViewController.swift
+//  TableView
+//
+//  Created by GSM08 on 2021/06/30.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    @IBOutlet var tfAddItem: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+    
+    }
+    
+}
