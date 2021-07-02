7a92a02ae50fe6b84c80e8af714c27019942deff
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-02 11:07:25 +0900

    MoviePlayer

---
+++ MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by GSM08 on 2021/07/02.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+    }
+    
+    private func playVideo(url: NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
+
