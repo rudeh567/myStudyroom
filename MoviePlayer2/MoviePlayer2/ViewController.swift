10d9f8d65a586c7b28e64d1e31ca1b3ef7e1ed39
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-03 17:48:32 +0900

    Video Player2

---
+++ MoviePlayer2/MoviePlayer2/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by GSM08 on 2021/07/02.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayInternalMovie2(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "Mountaineering", ofType: "mov")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayExternalMovie2(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/jiybpprsmx0bgre/Seascape.mov")!
+        
+        playVideo(url: url)
+    }
+    
+    
+    private func playVideo(url: NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
