042bb4b57acfed41cf413d98e854a7f6c8d678c0
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-05 17:40:19 +0900

    Gallery

---
+++ pageControl/pageControl/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  pageControl
+//
+//  Created by GSM08 on 2021/07/05.
+//
+
+import UIKit
+
+var images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]
+
+class ViewController: UIViewController {
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        imgView.image = UIImage(named: images[0])
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+    }
+    
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.left:
+                if (pageControl.currentPage<pageControl.numberOfPages-1) {
+                    pageControl.currentPage = pageControl.currentPage + 1
+                }
+                print("Swiped Left")
+            case UISwipeGestureRecognizer.Direction.right:
+                if (pageControl.currentPage>0) {
+                    pageControl.currentPage = pageControl.currentPage - 1
+                }
+                print("Swiped Right")
+            default:
+                break
+            }
+            
+            imgView.image = UIImage(named: images[pageControl.currentPage])
+        }
+    }
+    
+}
+
