779ef889f1343954f6628312be66f8b198c22c57
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-05 17:55:40 +0900

    Pinch Gesture 등록

---
+++ PinchGesture/PinchGesture/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PinchGesture
+//
+//  Created by GSM08 on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var txtPinch: UILabel!
+    
+    var initialFontSize:CGFloat!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
+    }
+
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        if (pinch.state == UIPinchGestureRecognizer.State.began) {
+            initialFontSize = txtPinch.font.pointSize
+        } else {
+            txtPinch.font = txtPinch.font.withSize(initialFontSize * pinch.scale)
+        }
+    }
+
+}
+
