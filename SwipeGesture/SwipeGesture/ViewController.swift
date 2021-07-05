9f1189951f9eec1f39e8cffecbea0e192e768072
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-05 17:17:37 +0900

    Swipe Gesture 2

--- SwipeGesture/SwipeGesture/ViewController.swift
+++ SwipeGesture/SwipeGesture/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let numOfTouches = 2
+    
     @IBOutlet var imgViewUp: UIImageView!
     @IBOutlet var imgViewDown: UIImageView!
     @IBOutlet var imgViewLeft: UIImageView!
@@ -24,12 +27,16 @@ class ViewController: UIViewController {
 
         imgUp.append(UIImage(named: "arrow-up-black.png")!)
         imgUp.append(UIImage(named: "arrow-up-red.png")!)
+        imgUp.append(UIImage(named: "arrow-up-green.png")!)
         imgDown.append(UIImage(named: "arrow-down-black.png")!)
         imgDown.append(UIImage(named: "arrow-down-red.png")!)
+        imgDown.append(UIImage(named: "arrow-down-green.png")!)
         imgLeft.append(UIImage(named: "arrow-left-black.png")!)
         imgLeft.append(UIImage(named: "arrow-left-red.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-green.png")!)
         imgRight.append(UIImage(named: "arrow-right-black.png")!)
         imgRight.append(UIImage(named: "arrow-right-red.png")!)
+        imgRight.append(UIImage(named: "arrow-right-green.png")!)
         
         imgViewUp.image = imgUp[0]
         imgViewDown.image = imgDown[0]
@@ -51,6 +58,26 @@ class ViewController: UIViewController {
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
         self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUpMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeUpMulti.direction = UISwipeGestureRecognizer.Direction.up
+        swipeUpMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeUpMulti)
+        
+        let swipeDownMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeDownMulti.direction = UISwipeGestureRecognizer.Direction.down
+        swipeDownMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeDownMulti)
+        
+        let swipeLeftMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeLeftMulti.direction = UISwipeGestureRecognizer.Direction.left
+        swipeLeftMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeLeftMulti)
+        
+        let swipeRightMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeRightMulti.direction = UISwipeGestureRecognizer.Direction.right
+        swipeRightMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeRightMulti)
     }
 
     @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
@@ -72,6 +99,29 @@ class ViewController: UIViewController {
             default:
                 break
             }
+        }
+    }
+    
+    @objc func respondToswipeGestureMulti(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            imgViewUp.image = imgUp[0]
+            imgViewDown.image = imgDown[0]
+            imgViewLeft.image = imgLeft[0]
+            imgViewRight.image = imgRight[0]
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.up:
+                imgViewUp.image = imgUp[2]
+            case UISwipeGestureRecognizer.Direction.down:
+                imgViewDown.image = imgDown[2]
+            case UISwipeGestureRecognizer.Direction.left:
+                imgViewLeft.image = imgLeft[2]
+            case UISwipeGestureRecognizer.Direction.right:
+                imgViewRight.image = imgRight[2]
+            default:
+                break
+            }
+            
         }
     }
 }
